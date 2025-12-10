e18d8b3ff1607bea35b19184f4551b507b63b465
abdallahkhaled2 <abdallahkhaled2@gmail.com>
2025-12-10 18:39:02 +0100

    Updated Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import App from './App';
 import Dashboard from './pages/Dashboard';
 import InvoicePreview from './InvoicePreview';
 
-const Router: React.FC = () => {
+const RouterContent: React.FC = () => {
+  const location = useLocation();
+  const showNavigation = location.pathname !== '/preview';
+
   return (
-    <BrowserRouter>
-      <Navigation />
+    <>
+      {showNavigation && <Navigation />}
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/preview" element={<InvoicePreview />} />
       </Routes>
+    </>
+  );
+};
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <RouterContent />
     </BrowserRouter>
   );
 };
